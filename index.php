921baa953d3d2b6dfd938ad4b758d62784e3b5ec
SophieElodie <sophie.slb@live.fr>
2017-03-30 10:26:39 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Ma page web</title>
+        <link rel="stylesheet" type="text/css" href="style/style.css">
+        <link rel="stylesheet" type="text/css" href="style/styleprofil.css">
+    </head>
+    <body>
+        <h1>Ma page web</h1>
+        <p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
+        <a href="monprofil.php"> Mon profil </a>
+    </body>
+</html>
+
---
+++ monprofil.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" type="text/css" href="style/styleprofil.css">
+<head>
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>
+
+<?php
+$nom= "LE BERRE";
+$prenom = "Sophie";
+$age = 20;
+echo "$nom </br> $prenom </br>  $age </br>";
+?>
