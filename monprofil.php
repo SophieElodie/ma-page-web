d1c1041ac0845ae1cdd217c8acadd22a81e7e1cf
SophieElodie <sophie.slb@live.fr>
2017-03-30 11:23:04 +0200

    ajout animation

--- monprofil.php
+++ monprofil.php
@@ -4,12 +4,14 @@
 <head>
 	<title></title>
 </head>
+<div id ="nom">
 <?php
 $nom= "LE BERRE";
 $prenom = "Sophie";
 $age = 20;
 echo "$nom $prenom </br>  $age ans </br>";
 ?>
+</div>
 <body>
 <div>
 <img src="sophie.jpg">
